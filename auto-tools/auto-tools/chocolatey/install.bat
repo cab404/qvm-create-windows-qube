523240e9519fbbf5a200a2efe564fe50699d9c2a
crazyqube <crazyqube@protonmail.com>
2019-08-29 10:13:38 +0000

    Set Chocolatey to be downloaded as a .zip instead of a .7z

--- auto-tools/auto-tools/chocolatey/install.bat
+++ auto-tools/auto-tools/chocolatey/install.bat
@@ -2,4 +2,5 @@
 title %~f0
 
 rem Command provided by: https://chocolatey.org/install
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+rem Added environment variable to use Windows compression so 7-Zip is not a mandatory install
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "$env:chocolateyUseWindowsCompression = 'true'; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
