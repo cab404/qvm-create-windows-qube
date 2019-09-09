acc8c8a6273fc764847f68d9b83275c73dd72d5b
crazyqube <crazyqube@protonmail.com>
2019-09-09 04:44:58 +0000

    Slow down run.bat child process scanning and void output of wmic

--- auto-tools/auto-tools/run.bat
+++ auto-tools/auto-tools/run.bat
@@ -16,7 +16,7 @@ start cmd /c "cd updates && "install-updates.bat""
 rem If the PPID of any cmd.exe is equal to the PID of this process we will wait for them to exit and then shutdown
 for /f "tokens=2 usebackq" %%p in (`tasklist /v /fi "IMAGENAME eq cmd.exe" ^| findstr /l %~f0`) do set PID=%%p
 :jobs_running
-wmic process where (parentprocessid="%PID%" and name="cmd.exe") | findstr /l "No Instance(s) Available."
-if %ERRORLEVEL%==0 timeout 1 /nobreak && goto jobs_running
+wmic process where (parentprocessid="%PID%" and name="cmd.exe") | findstr /l "No Instance(s) Available." >nul
+if %ERRORLEVEL%==0 timeout 5 /nobreak && goto jobs_running
 
 shutdown /s /t 0
